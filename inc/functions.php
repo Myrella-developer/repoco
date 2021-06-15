d4936e47564fe82b285ff6b86d16bae4892a372d
Genis98 <genisgcuyas@gmail.com>
2021-06-15 13:50:47 +0200

    funcionalidad recPass

--- inc/functions.php
+++ inc/functions.php
@@ -15,4 +15,34 @@ function desconectar($conexion){
     mysqli_close($conexion);
 }
 
+function sendMail($mailTo, $asunto, $body){
+    require_once("../inc/phpmailer.class.php");
+    
+    $mail = new PHPMailer();
+    $mail -> IsSMTP();
+    $mail -> SMTPAuth = true;
+    $mail -> SMTPSecure = "tls";
+    $mail -> Host = "smtp.dondominio.com";
+    $mail -> Port = 587;
+    $mail -> Username = "genis@cobd.es";
+    $mail -> Password = "Genis_3142";
+    $mail -> FromName = "Genis";
+    $mail -> From = "genis@cobd.es";
+    $mail -> SMTPDebug = 1; 
+    $mail -> SMTPDebug = 2; 
+    $mail -> AddReplyTo("genis@cobd.es");
+    $mail -> AddAddress($mailTo);
+    $mail -> Timeout = 100;
+    $mail -> IsHtml(true);
+    $mail -> CharSet = "UTF-8";
+    $mail -> Subject = $asunto;
+    $mail -> Body = $body;
+    $mail -> AltBody = $body;
+    
+    $exito = $mail -> Send();
+    $mail -> ClearAddresses();
+    if(!$exito) return $mail->ErrorInfo;
+    else echo "Mail enviado";
+}
+
 ?>

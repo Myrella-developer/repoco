3c23064accb90851f4fb0ee03b5cafa15511b2e3
Genis98 <genisgcuyas@gmail.com>
2021-06-15 11:30:19 +0200

    arreglao

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let app= angular.module('repoco',['ngRoute']);
 app.config(['$routeProvider',function($routeProvider){
 	$routeProvider
 	.when('/',{
-		templateUrl:'./index.html',
+		templateUrl:'../index.html',
 		controller:'IndexController'
 	})
 	.when('/home',{

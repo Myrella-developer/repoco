3c2f8db604ff6ca17c75c90e2f97a9106de7bd3e
FedericoMartin93 <f.mmateu93@gmail.com>
2021-06-15 14:40:47 +0200

    versión estable

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,22 +1,22 @@
 angular.module("repoco")
 .controller("IndexController", function($scope, $http, $q, $rootScope){
 	 //   console.log("dasd");
-	$http.post("models/index.php")
+	// $http.post("models/index.php")
 
-	.then((res)=>{
-		defered.resolve(res);
-	})
+	// .then((res)=>{
+	// 	defered.resolve(res);
+	// })
 
-	.catch((err)=>{console.log(err.statusText)})
+	// .catch((err)=>{console.log(err.statusText)})
 
-	.finally(()=>{});
+	// .finally(()=>{});
 })
 
 .controller("LoginController", function($scope, $http, $q, $location, $rootScope){
 	console.log("Carga controller login");
 	$scope.email = "pancracio@gmail.com";
 	$scope.pass = "1111";
-	$$scope.tipo = "a"
+	$scope.tipo = "a"
 	$scope.errorLogin=false;
 	$scope.entrar = function(){
 		let data = new FormData()

43ce14ccce5b26c7913e29ce68e88de36a23a0a5
Genis98 <genisgcuyas@gmail.com>
2021-06-16 14:41:34 +0200

    añado modals a edicio y projectes

--- views/ediciones.php
+++ views/ediciones.php
@@ -1,15 +1,49 @@
 <div class="container-fluid">
-    <h2>Edicions</h2>
-    
+    <h2 class="text-center mt-5">Edicions</h2>
+
     <i class="fas fa-plus-square ms-4 mt-5 text-primary"></i>
-    <label class="ms-1 fw-bold">AFEGIR</label>
+    <label class="ms-1 fw-bold" data-bs-toggle="modal" data-bs-target="#afegir">AFEGIR</label>
+
+    <div class="modal fade" id="afegir" tabindex="-1" aria-labelledby="labelDigital" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="labelDigital">afegir</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    body
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Dessar canvis</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div class="row">
         <div class="card cardGestor m-5" style="width: 18rem;" ng-repeat="edicio in edicions">
             <div class="card-body">
                 <h5 class="card-title">{{edicio.nom}}</h5>
                 <p class="card-text">{{edicio.dataInici}} - {{edicio.dataFi}}</p>
-                <a href="#" class="btn btn-primary">Modificar</a>
+                <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#gestor">Modificar</a>
+            </div>
+        </div>
+
+        <div class="modal fade" id="gestor" tabindex="-1" aria-labelledby="labelDigital" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="labelDigital">Titulo</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        body
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Dessar canvis</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
